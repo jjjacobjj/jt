839b70990dcb46387a10531fb76eedfb3c0e03a6
jjjacobjj <jacobjosephjohnson@gmail.com>
2025-09-20 15:53:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,24 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/jt.db'
+app.config['SECRET_KEY'] = 'supersecretkey'
+
+# Make sure the instance folder exists
+if not os.path.exists('instance'):
+    os.makedirs('instance')
+
+# SQLite database path
+db_path = os.path.join('instance', 'jt.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
-# Database Models
+# Models
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
 
 class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,7 +30,11 @@ class Video(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user = db.relationship('User', backref=db.backref('videos', lazy=True))
 
-# Homepage
+# Create tables if they don't exist
+with app.app_context():
+    db.create_all()
+
+# Routes
 @app.route('/')
 def index():
     videos = Video.query.all()
